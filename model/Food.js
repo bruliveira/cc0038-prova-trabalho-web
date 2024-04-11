0c6c503abe32c908ae032d90aa554456327029f3
bruliveira <brunaflavia910@gmail.com>
2024-04-11 02:06:02 -0300

    :penguin: Add moods - Food

---
+++ model/Food.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const FoodSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    expirationDate: {
+        type: Date,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Food', FoodSchema);
